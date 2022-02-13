2d0730c18bc5a188aaf71ed8a12823fc34d52fa9
SAYFIDDIN <isco6323@gmail.com>
2022-02-13 14:14:13 +0500

    new config

--- data/config.py
+++ data/config.py
@@ -1,11 +1,20 @@
-from environs import Env
+# from environs import Env
+#
+# # environs kutubxonasidan foydalanish
+# env = Env()
+# env.read_env()
+#
+# # .env fayl ichidan quyidagilarni o'qiymiz
+# BOT_TOKEN = env.str("BOT_TOKEN")  # Bot toekn
+# ADMINS = env.list("ADMINS")  # adminlar ro'yxati
+# IP = env.str("ip")  # Xosting ip manzili
+# CHANNELS = ['-1001657087486']
 
-# environs kutubxonasidan foydalanish
-env = Env()
-env.read_env()
+
+import os
 
 # .env fayl ichidan quyidagilarni o'qiymiz
-BOT_TOKEN = env.str("BOT_TOKEN")  # Bot toekn
-ADMINS = env.list("ADMINS")  # adminlar ro'yxati
-IP = env.str("ip")  # Xosting ip manzili
+BOT_TOKEN = str(os.environ.get("BOT_TOKEN"))  # Bot token
+ADMINS = list(os.environ.get("ADMINS"))  # adminlar ro'yxati
+IP = str(os.environ.get("ip"))  # Xosting ip manzili
 CHANNELS = ['-1001657087486']
